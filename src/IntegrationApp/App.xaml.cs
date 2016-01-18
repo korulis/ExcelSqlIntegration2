e29f412ab25bd8823edd0fbd37ccaf4f39db1b83
korulis <broliukas@gmail.com>
2016-01-18 15:02:27 +0200

    reset: delete test project

--- src/IntegrationApp/App.xaml.cs
+++ src/IntegrationApp/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -11,7 +12,25 @@ namespace IntegrationApp
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
+
+    
     public partial class App : Application
     {
+        public App()
+        {
+            MainWindow = new MainWindow();
+        }
+
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+        }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow.Show();
+            //MainWindow = new MainWindow();
+            //MainWindow = new MainWindow();
+            //this.StartupUri = new System.Uri("MainWindow.xaml", System.UriKind.Relative);
+        }
     }
 }
